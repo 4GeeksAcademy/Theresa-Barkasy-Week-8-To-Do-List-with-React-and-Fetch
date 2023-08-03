b43a104149f4e2bb9fc6ff0a8e917e932e9b09be
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-08-03 00:07:44 +0000

    css added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,15 @@
 import React, {useState} from "react";
+import Header from "./header";
 import NewToDo from "./newtodo"; 
-import MapFunction from "./mapFunction";
-// import ToDoCounter from "./todocounter";
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">to do</h1>
+			<Header />
 			<ul>
 				<NewToDo />
-				{/* <MapFunction /> */}
-				{/* <ToDoCounter /> */}
 			</ul>
 		</div>
 	);
